6820a595f89e0b14feef2189de6e2992ad1978f7
Tim Murray <timmurray@google.com>
2016-01-19 16:35:17 -0800

    Minor fixes

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Architecture
+export ARCH=arm64
+export TARGET_ARCH=arm64
+make O=out cappu_defconfig
+export KBUILD_BUILD_USER=goayandi
+export KBUILD_BUILD_HOST=universal
+export CROSS_COMPILE=`pwd`/toolchain/bin/aarch64-linux-android-
+make O=out Image.gz-dtb
