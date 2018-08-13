2268d23fba60295c78126fd514edad90e62f719a
Goayandi <r.borutha@gmail.com>
2018-08-13 12:54:57 +0200

    Build-Script: Change toolchain

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ export TARGET_ARCH=arm64
 make O=out cappu_defconfig
 export KBUILD_BUILD_USER=goayandi
 export KBUILD_BUILD_HOST=universal
-export CROSS_COMPILE=`pwd`/toolchain/bin/aarch64-linux-android-
+export CROSS_COMPILE=`pwd`/toolchain/bin/aarch64-linux-gnu-
 make O=out Image.gz-dtb -j6
